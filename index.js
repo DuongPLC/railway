94d56fbd652c5f77d25a6fce875e84ee8046bbfc
DuongPLC <nguyenvanduongns@gmail.com>
2025-06-02 14:32:37 +0700

    add Dockerfile to fix playwright dependency issue

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/getdata', async (req, res) => {
     const browser = await chromium.launch({ headless: true });
     const page = await browser.newPage();
-
-    await page.goto('https://esinad.minedu.gob.pe/e_sinadmed_1/resolucionesexternas/consultanormas.aspx');
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36');
+    await page.goto('https://esinad.minedu.gob.pe/e_sinadmed_1/resolucionesexternas/consultanormas.aspx', { waitUntil: 'load' });
+    // await page.goto('https://esinad.minedu.gob.pe/e_sinadmed_1/resolucionesexternas/consultanormas.aspx');
 
     await page.fill('#ctl00_ContentPlaceHolder1_txtNumResol', '077-2025-MINEDU');
     await page.fill('#ctl00_ContentPlaceHolder1_txtFechaDesde', '01/02/2025');
